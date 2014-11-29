65a5c0c6279a42d12677f29e52a415c1f89cd750
Nicholas Hare <oharemarketing@gmail.com>
2014-11-29 11:08:40 -0500

    auth JS moved to js file

---
+++ fbAuth.js
@@ -0,0 +1,59 @@
+//
+// var activApp = angular.module("activApp", ["firebase"]);
+//   activApp.controller('fbAuth', ['$scope', '$firebase',
+//     function($scope, $firebase) {
+//       var activ = new Firebase('https://activ8.firebaseio.com/');
+//       // var auth = new FirebaseSimpleLogin(activ, function(error, user) {
+//       activ.authWithOAuthRedirect("facebook", function(error) {
+//         remember: "default"
+//         if (error) {
+//           // an error occurred while attempting login
+//           console.log(error);
+//         } else if (user) {
+//           // user authenticated with Firebase
+//           console.log('User ID: ' + user.id + ', Provider: ' + user.provider);
+//         } else {
+//           // user is logged out
+//         }
+//       });
+//     }
+//   ]);
+
+angular.module('activ8', ['firebase'])
+  .controller('MainController', function($firebase, $firebaseAuth, $scope){
+    var fire = new Firebase('https://activ8.firebaseio.com/'),
+        sync = $firebase(fire),
+        fbAuth = $firebaseAuth(fire),
+  syncObject = sync.$asObject(),
+        self = this;
+
+
+    syncObject.$bindTo($scope, "data");
+    $scope.workouts = sync.$asArray();
+
+    var actObj = $firebase(fire).$asObject();
+
+    $scope.login = function(){
+      console.log(fbAuth)
+      fbAuth.$authWithOAuthPopup("facebook").then(function(authData){
+        console.log("Logged in as: ", authData.facebook.displayName);
+        self.displayName = authData.facebook.displayName;
+        self.profilePic = authData.facebook.cachedUserProfile.picture.data.url;
+      }).catch(function(error){
+        console.log("Authentication failed: ", error);
+      });
+    }
+
+    $scope.loggedIn = function(){
+      if(fbAuth.$getAuth() != null){
+        return true;
+      }
+    }
+
+    $scope.logOut = function(){
+      fbAuth.$unauth()
+      window.location.reload()
+    }
+
+    var movesAdded = [ ];
+  });
