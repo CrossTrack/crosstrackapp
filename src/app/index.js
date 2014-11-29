7db09ac4f5dfc2b5e45b1a18998d9639310677d9
fab9 <rorou192@gmail.com>
2014-11-28 19:17:48 -0500

    fab9: add routes

--- src/app/index.js
+++ src/app/index.js
@@ -1,18 +1,29 @@
 'use strict';
 
-
 var app = angular.module('crosstrack',
-  ['firebase', 'ngAnimate', 'ngCookies', 'ngTouch', 'ngResource', 'ngRoute'])
+  ['firebase', 'ngAnimate', 'ngCookies', 'ngTouch', 'ngResource', 'ngRoute']);
 
-  // .constant('FIREBASE_URL', 'https://crosstrack.firebaseio.com/');
-  app.config(function ($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'app/main/main.html',
-        controller: 'MainCtrl'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
-  })
-;
+var appControllers = angular.module('appControllers', ['firebase']);
+
+app.config(['$routeProvider', function($routeProvider) {
+  $routeProvider.
+    when('/', {
+      templateUrl: 'app/main/main.html',
+      controller:  'MainCtrl'
+    }).
+    when('/dashboard', {
+      templateUrl: 'app/views/dashboard.html',
+      controller:  'DashboardCtrl'
+    }).
+    when('/new-workout', {
+      templateUrl: 'app/views/new-workout.html',
+      controller:  'NewWorkoutCtrl'
+    }).
+    when('/history', {
+      templateUrl: 'app/views/history.html',
+      controller:  'HistoryCtrl'
+    }).
+    otherwise({
+      redirectTo: '/'
+    });
+}]);
