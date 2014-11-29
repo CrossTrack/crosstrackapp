f45807af923e3c022a4f9616964f545c1511781b
fab9 <rorou192@gmail.com>
2014-11-28 19:24:25 -0500

    fab9: create files for all controllers

---
+++ src/app/controllers/new-workout.controller.js
@@ -0,0 +1,11 @@
+app.controller("NewWorkoutCtrl", ["$scope", "$firebase",
+  function($scope, $firebase) {
+    var ref = new Firebase("https://crosstrack.firebaseio.com/");
+
+    // create an AngularFire reference to the data
+    var sync = $firebase(ref);
+
+    // download the data into a local object
+    $scope.data = sync.$asObject();
+  }
+]);
