f9480bf8747afc101df62893b8d9205c3c8d09db
fab9 <rorou192@gmail.com>
2014-11-29 20:43:20 -0500

    fab9: save logged in users to Firebase fab9: save logged in users to Firebase fab9: save logged in users to Firebase fab9: save logged in users to Firebase fab9: save logged in users to Firebase fab9: save logged in users to Firebase fab9: save logged in users to Firebase fab9: save logged in users to Firebase fab9: save logged in users to Firebase

--- src/app/controllers/main.controller.js
+++ src/app/controllers/main.controller.js
@@ -1,10 +1,9 @@
 'use strict';
-/*global Firebase */
+/* global Firebase */
 
 app.controller('MainCtrl', function ($scope, $firebase, Auth) {
   $scope.auth = Auth;
   $scope.user = $scope.auth.$getAuth();
-
   $scope.login = function authenticate(e) {
       var ref = new Firebase('https://crosstrack.firebaseio.com/');
       var uid = null;
@@ -22,6 +21,20 @@ app.controller('MainCtrl', function ($scope, $firebase, Auth) {
 
         console.log('User logged in with id: ', uid);
         console.log('username: ', username);
+        // new code
+        // we would probably save a profile when we register new users on our site
+        // we could also read the profile to see if it's null
+        // here we will just simulate this with an isNewUser boolean
+        var isNewUser = true;
+
+        var ref = new Firebase("https://crosstrack.firebaseio.com");
+        ref.onAuth(function(authData) {
+          if (authData && isNewUser) {
+            // save the user's profile into Firebase so we can list users,
+            // use them in Security and Firebase Rules, and show profiles
+            ref.child('users').child(authData.uid).set(authData);
+          }
+        }); //end new code
 
       },
       {
