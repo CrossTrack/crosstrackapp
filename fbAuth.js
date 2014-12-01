72b60e351746b3e7f5ff214793fa1a5a2664694b
Nicholas Hare <oharemarketing@gmail.com>
2014-12-01 17:09:15 -0500

    trying to store user data

--- fbAuth.js
+++ fbAuth.js
@@ -1,67 +1,50 @@
-//
-// var activApp = angular.module("activApp", ["firebase"]);
-//   activApp.controller('fbAuth', ['$scope', '$firebase',
+'use strict';
 
-  var ref = new Firebase("https://activ8.firebaseio.com");
+angular.module('activ8', ['firebase'])
+  .controller('MainController', function($firebase, $firebaseAuth, $scope){
+    var fire = new Firebase('https://activ8.firebaseio.com/'),
+        users = { },
+        sync = $firebase(fire),
+        fbAuth = $firebaseAuth(fire),
+        syncObject = sync.$asObject(),
+        self = this;
 
-  var auth = new FirebaseSimpleLogin(ref, function(error, user) {
-    // ref.onAuth(function(authData) {
-      if (error) {
-        console.log(error);
-      } else if (user) {
-        // user authenticated with Firebase
-        console.log("User ID: " + user.uid + ", Provider: " + user.provider);
-      } else {
-        // user is logged out
-        console.log("Not logged in!");
-      }
-    // });
-  });
+    syncObject.$bindTo($scope, "data");
+    $scope.workouts = sync.$asArray();
 
-  $(function(){
-    $("#fBlogin").click(function(){
-      auth.login('facebook', {
-        rememberMe: true
+    var actObj = $firebase(fire).$asObject();
+
+    // $scope.newUser = function(){
+    //  fire.child('users').child(authData.uid).set(authData);
+    // }
+
+    $scope.login = function(){
+      fbAuth.$authWithOAuthPopup("facebook").then(function(authData){
+        console.log("Logged in as: ", authData.uid);
+        self.displayName = authData.facebook.displayName;
+        self.profilePic = authData.facebook.cachedUserProfile.picture.data.url;
+        // users.child(authData.uid).once('value', function(snapshot) {
+        //   if (snapshot.val() === null) {
+        //     console.log("New User");
+        //   } else {
+        //     console.log("User exists");
+        //   }
+        // });
+      }).catch(function(error){
+        console.log("Authentication failed: ", error);
       });
-    });
+    }
+
+    $scope.loggedIn = function(){
+      if(fbAuth.$getAuth() != null){
+        return true;
+      }
+    }
+
+    $scope.logOut = function(){
+      fbAuth.$unauth();
+      window.location.reload()
+    }
+
+    var movesAdded = [ ];
   });
-
-
-// angular.module('activ8', ['firebase'])
-//   .controller('MainController', function($firebase, $firebaseAuth, $scope){
-//     var fire = new Firebase('https://activ8.firebaseio.com/'),
-//         sync = $firebase(fire),
-//         fbAuth = $firebaseAuth(fire),
-//   syncObject = sync.$asObject(),
-//         self = this;
-//
-//
-//     syncObject.$bindTo($scope, "data");
-//     $scope.workouts = sync.$asArray();
-//
-//     var actObj = $firebase(fire).$asObject();
-//
-//     $scope.login = function(){
-//       console.log(fbAuth)
-//       fbAuth.$authWithOAuthPopup("facebook").then(function(authData){
-//         console.log("Logged in as: ", authData.facebook.displayName);
-//         self.displayName = authData.facebook.displayName;
-//         self.profilePic = authData.facebook.cachedUserProfile.picture.data.url;
-//       }).catch(function(error){
-//         console.log("Authentication failed: ", error);
-//       });
-//     }
-//
-//     $scope.loggedIn = function(){
-//       if(fbAuth.$getAuth() != null){
-//         return true;
-//       }
-//     }
-//
-//     $scope.logOut = function(){
-//       fbAuth.$unauth()
-//       window.location.reload()
-//     }
-//
-//     var movesAdded = [ ];
-//   });
