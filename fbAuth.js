d35c64d520a4d60d66f9982c6cec50b804377163
Nicholas Hare <oharemarketing@gmail.com>
2014-12-01 21:34:09 -0500

    working on users

--- fbAuth.js
+++ fbAuth.js
@@ -3,7 +3,6 @@
 angular.module('activ8', ['firebase'])
   .controller('MainController', function($firebase, $firebaseAuth, $scope){
     var fire = new Firebase('https://activ8.firebaseio.com/'),
-        users = { },
         sync = $firebase(fire),
         fbAuth = $firebaseAuth(fire),
         syncObject = sync.$asObject(),
@@ -13,6 +12,8 @@ angular.module('activ8', ['firebase'])
     $scope.workouts = sync.$asArray();
 
     var actObj = $firebase(fire).$asObject();
+
+    var users = new Firebase('https://activ8.firebaseio.com/web/data/users/');
 
     // $scope.newUser = function(){
     //  fire.child('users').child(authData.uid).set(authData);
@@ -24,11 +25,11 @@ angular.module('activ8', ['firebase'])
         self.uid = authData.uid;
         self.displayName = authData.facebook.displayName;
         self.profilePic = authData.facebook.cachedUserProfile.picture.data.url;
-        users.once('value', function(snapshot) {
-          snapshot.childExists(self.uid, function(exists) {
-            console.log('user exists');
-          });
-        });
+        // if(users.once('value', function(snapshot) {
+        //   snapshot.childExists(self.uid, function(exists) {
+        //     console.log('user exists');
+        //   });
+        // }));
       }).catch(function(error){
         console.log("Authentication failed: ", error);
       });
