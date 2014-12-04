654f61752215561cca8670652dcdcf71e5bfd135
LoganArnett <Loganarn5@gmail.com>
2014-12-04 14:15:19 -0500

    added angular add movement selection

--- logan/app/main/add-workout-controller.js
+++ logan/app/main/add-workout-controller.js
@@ -5,7 +5,7 @@ angular.module('activ8')
 .controller('WorkoutController', function(){
   var self = this,
   movements = [
-    {
+   {
       name: "Pull Ups",
       reps: 1,
       rds: 1
@@ -17,10 +17,29 @@ angular.module('activ8')
     }
   ];
 
-  this.moveData = {};
-  this.moveData.movements = movements;
+  this.moveList = {};
+  this.moveList.movements = movements;
+
+   this.addMove = function(){
+    $('.new-moves').append('<input type="text" data-ng-model="moveList.movements.name" list="movename">')
+  };
 
   // this.addWorkout = function(){
   //   console.log("Hello")
   // }
 })
+
+
+// <form>
+// <div>
+// <label>title</label>
+// <input type="text" data-ng-model="item.title" list="comicstitle">
+// </div>
+// <div>
+// <input type="Button" value="Add" data-ng-click="addItem(item)">
+// </div>
+// </form>
+// </div>
+// <datalist id="comicstitle">
+// <option  data-ng-repeat="ttl in titles" value="{{ttl}}">
+// </datalist>
