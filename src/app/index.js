09cbebd18621186754eba46da9e316bee74d0186
fab9 <rorou192@gmail.com>
2014-12-05 10:54:29 -0500

    fab9: newly generated Yeoman project

---
+++ src/app/index.js
@@ -0,0 +1,14 @@
+'use strict';
+
+angular.module('activ8', ['ngRoute'])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'app/main/main.html',
+        controller: 'MainCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  })
+;
