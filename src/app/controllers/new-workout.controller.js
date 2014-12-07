08ca24da445c9359993c6714dcfceca5d0a6330b
LoganArnett <Loganarn5@gmail.com>
2014-12-07 11:39:14 -0500

    able to add multiple moves dynamically and submit the form to firebase

--- src/app/controllers/new-workout.controller.js
+++ src/app/controllers/new-workout.controller.js
@@ -43,94 +43,67 @@ angular.module('activ8')
     type: 2
   }
   };
+
+  //Posting Workouts to the Workout Array
   this.workout = [ ];
-  this.move = {name: " ", reps: " ", rds: " ", weight: " "};
+
+  this.addMove = function(){
+
+    this.workout.push({
+      name: "",
+      reps: "",
+      rds: "",
+      weight: "",
+      moveNumber: this.workout.length + 1
+    });
+
+  };
+
+  // Utilize the Date for titles?
   this.date = Date.now();
   var sync = $firebase(ref.child(Date.now()))
 
-   this.newWorkout = sync.$asArray();
-  this.addWork = function(user, move, move2) {
-    this.newWorkout.$add(
-      {
-       user: Auth.getUser().uid,
-       move1: move.name,
-       move1reps: move.reps,
-       move1rds: move.rds,
-       move1weight: move.weight,
-       move2: move2.name,
-       move2reps: move2.reps,
-       move2rds: move2.rds,
-       move2weight: move2.weight
+  this.newWorkout = sync.$asArray();
+  this.addWork = function(workout){
+    this.newWorkout.$add(workout);
+  }
 
-     });
-  };
-
-
-  console.log(movements["PullUps"].type)
-  // Utilize the Date for titles?
-
-
-  //Posting Workouts to the Workout Array
-
-
-
-  // this.postWorkout = function (){
-  //   this.workout.push(this.move);
-  //   this.move = {name: " ", reps: " ", rds: " ", weight: " "};
-  //   console.log("Poo")
-  // };
-
-
-  var userWorkouts = $firebase(ref.child('workouts').child(this.date.toString())).$asObject()
-
-  this.postWorkout = function(move){
-    userWorkouts.$push;
-  };
 
   this.moveList = {};
   this.moveList.movements = movements;
-  console.log(Auth.getUser())
-  // (movements[($('.moving').val().replace(/ /g, ''))].type === 1)
+
+  //Working on displaying the right properties of each movement
   this.repsRounds = function(){
-    if($('.moving').val() === ""){
+    if($('.naming1').val() === ""){
       return false;
     }
-    else if((movements[($('.moving').val().replace(/ /g, ''))].type === 1)){
+    else if($('.naming1').val()){
       return true;
     }
   }
+  //Checking the Movement Value to display proper inputs
   this.weighted = function(){
-  if($('.moving').val() === ""){
-    return false;
-  }
-  else if((movements[($('.moving').val().replace(/ /g, ''))].type === 2)){
+  if($(".naming1").val().indexOf("Clean") > -1){
     return true;
   }
-}
-
-   this.addMove = function(){
-    $('.new-moves').append(
-    '<input class="moving" ng-model="move2.name" type="text" list="moving" placeholder="Select Movement" autocomplete><br>' +
-    '<input type="text" ng-model="move2.reps" ng-show="work.repsRounds() || work.weighted()" placeholder="Reps"><br>' +
-    '<input type="text" ng-model="move2.rds" ng-show="work.repsRounds() || work.weighted()" placeholder="Rounds"><br>' +
-    '<input type="text" ng-model="move2.weight"ng-show="work.weighted()" placeholder="Weight"><br>');
-  };
-  // this.addWorkout = function(){
-  //   console.log("Hello")
+  // else if($(".naming1").val().indexOf("Snatch") > -1){
+  //   return true;
   // }
-})
+  // else if($(".naming1").val().indexOf("Jerk") > -1){
+  //   return true;
+  // }
+  // else if($(".naming1").val().indexOf("Press") > -1){
+  //   return true;
+  // }
+  // else if($(".naming1").val().indexOf("Kettle") > -1){
+  //   return true;
+  // }
+  // else if($(".naming1").val().indexOf("Dumbbell") > -1){
+  //   return true;
+  // }
+  else {
+    return false;
+   }
+  }
 
-
-// <form>
-// <div>
-// <label>title</label>
-// <input type="text" data-ng-model="item.title" list="comicstitle">
-// </div>
-// <div>
-// <input type="Button" value="Add" data-ng-click="addItem(item)">
-// </div>
-// </form>
-// </div>
-// <datalist id="comicstitle">
-// <option  data-ng-repeat="ttl in titles" value="{{ttl}}">
-// </datalist>
+});//End Controller
