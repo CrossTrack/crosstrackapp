bfb4156b30e70283d5d756a78046893f51d20e2c
LoganArnett <Loganarn5@gmail.com>
2014-12-08 19:37:28 -0500

    merge conflicts resolved

--- src/app/controllers/history.controller.js
+++ src/app/controllers/history.controller.js
@@ -9,11 +9,7 @@ angular.module('activ8')
 
   var historyArray = sync.$asArray();
 
-<<<<<<< HEAD
-   this.historyList = historyArray
-=======
   this.historyList = historyArray
->>>>>>> aa41474ff553f806e135f44c0180568f8cf0d696
 
 
 
