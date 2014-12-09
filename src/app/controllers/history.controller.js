0cf05cb764aad3f73f009f9112ff920d21a8508c
LoganArnett <Loganarn5@gmail.com>
2014-12-09 10:36:03 -0500

    updating history controller

--- src/app/controllers/history.controller.js
+++ src/app/controllers/history.controller.js
@@ -5,12 +5,8 @@ angular.module('activ8')
 .controller('HistoryController', function(Auth, $firebase){
   var ref = new Firebase("https://activ8.firebaseio.com/workouts/" + Auth.getUser().uid).orderByKey().limitToLast(3);
 
-  var sync = $firebase(ref);
+  var sync = $firebase(ref).$asArray();
 
-  var historyArray = sync.$asArray();
-
-  this.historyList = historyArray
-
-
+  this.historyList = sync
 
 });
