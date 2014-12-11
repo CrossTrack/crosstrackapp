58dd0531611d048926ab976292b32e32cddf48dc
LoganArnett <Loganarn5@gmail.com>
2014-12-11 00:54:31 -0500

    more moves

--- src/app/controllers/new-workout.controller.js
+++ src/app/controllers/new-workout.controller.js
@@ -42,7 +42,119 @@ angular.module('activ8')
     "HangSquatClean": {
       name: "Hang Squat Clean",
       type: 2
-    }
+    },
+    "PowerSnatch": {
+      name: "Power Snatch",
+      type: 2
+    },
+    "SquatSnatch": {
+      name: "Squat Snatch",
+      type: 2
+    },
+    "HangPowerSnatch": {
+      name: "Hang Power Snatch",
+      type: 2
+    },
+    "HangSquatSnatch": {
+      name: "Hang Squat Snatch",
+      type: 2
+    },
+    "Clean&Jerk": {
+      name: "Clean & Jerk",
+      type: 2
+    },
+    "PushJerk": {
+      name: "Push Jerk",
+      type: 2
+    },
+    "Jerk": {
+      name: "Jerk",
+      type: 2
+    },
+    "SplitJerk": {
+      name: "Split Jerk",
+      type: 2
+    },
+    "Squat": {
+      name: "Squat",
+      type: 2
+    },
+    "OverheadSquat": {
+      name: "Overhead Squat",
+      type: 2
+    },
+    "FrontSquat": {
+      name: "Front Squat",
+      type: 2
+    },
+    "Deadlift": {
+      name: "Deadlift",
+      type: 2
+    },
+    "SumoDeadlift": {
+      name: "Sumo Deadlift",
+      type: 2
+    },
+    "MuscleUps": {
+      name: "Muscle Ups",
+      type: 1
+    },
+    "BarMuscleUps": {
+      name: "Bar Muscle Ups",
+      type: 1
+    },
+    "RingRows": {
+      name: "Ring Rows",
+      type: 1
+    },
+    "Pistol Squats": {
+      name: "Pistol Squats",
+      type: 1
+    },
+    "KettlebellSwingsAmerican": {
+      name: "Kettlebell Swings American",
+      type: 1
+    },
+    "KettlebellSwingsRussian": {
+      name: "Kettlebell Swings Russian",
+      type: 1
+    },
+    "TireFlips": {
+      name: "Tire Flips",
+      type: 1
+    },
+    "BearCrawls": {
+      name: "Bear Crawls",
+      type: 1
+    },
+    "Thrusters": {
+      name: "Thrusters",
+      type: 1
+    },
+    "Clusters": {
+      name: "Clusters",
+      type: 1
+    },
+    "StrictPress": {
+      name: "Strict Press",
+      type: 1
+    },
+    "PushPress": {
+      name: "Push Press",
+      type: 1
+    },
+    "BenchPress": {
+      name: "Bench Press",
+      type: 1
+    },
+    "RopeClimbs": {
+      name: "Rope Climbs",
+      type: 1
+    },
+    "WallBalls": {
+      name: "Wall Balls",
+      type: 1
+    },
   };
 
   //Posting Workouts to the Workout Array
